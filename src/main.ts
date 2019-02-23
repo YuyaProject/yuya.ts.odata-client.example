b5ffa92f099ca3b5376b3276943a95f0c76335ec
Alper Konuralp <alper@konuralp.gen.tr>
2019-02-23 16:15:28 +0300

    I added People list from https://services.odata.org/TripPinRESTierService/People in Home.vue

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { DefaultConnectionService } from 'yuya.ts.odata-client';
 
 Vue.config.productionTip = false;
+DefaultConnectionService.config.baseUrl = 'https://services.odata.org/';
+DefaultConnectionService.config.odataEndpoint = 'TripPinRESTierService';
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <table align="center">
+      <caption>People</caption>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>User Name</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, index) in data" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ row.UserName }}</td>
+          <td>{{ row.FirstName }}</td>
+          <td>{{ row.LastName }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { ODataQueryBuilder } from 'yuya.ts.odata-client';
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+  private data: any[] = [];
+
+  private created() {
+    new ODataQueryBuilder('People')
+      .addColumns('UserName', 'FirstName', 'LastName')
+      .q()
+      .then((d: any) => {
+        if (!!d && !!d.value) {
+          this.data = d.value;
+        }
+      });
+  }
+}
 </script>
